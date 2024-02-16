7d8646b7fcd719e6d1e5976e685649117ac56936
Anna Tsepilova <virgo.tsav@gmail.com>
2024-02-16 18:43:29 +0200

    add task-01,02

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,51 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
 import css from './App.module.css';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactsList from 'components/ContactsList/ContactsList';
+import Section from 'components/Section/Section';
 
 class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  addContact = contact => {
+    const isInContacts = this.state.contacts.some(
+      ({ name }) => name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isInContacts) {
+      alert(`${contact.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
+    }));
+  };
+
+  formSubmitHandler = data => {
+    let id = nanoid();
+    let contact = { id: id, name: data.name, number: data.number };
+
+    // let isContact = this.state.contacts.filter(contact =>
+    //   contact.name.toLowerCase().includes(data.name.toLowerCase())
+    // );
+    // console.log(isContact);
+    // if (isContact.length) {
+    //   Notify.warning(`${data.name} is already in contacts`, {
+    //     background: '#eebf31',
+    //     fontSize: '16px',
+    //     width: '350px',
+    //   });
+    //   return;
+    // }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
   };
 
   render() {
@@ -14,12 +53,14 @@ class App extends Component {
       <section className={css.sectionWrapper}>
         <h1 className={css.title}>Phonebook</h1>
         <Section>
-          <ContactForm />
+          <ContactForm onSubmit={this.formSubmitHandler} />
         </Section>
         <Section title="Contacts">
-          <ContactsList />
+          <ContactsList contacts={this.state.contacts} />
         </Section>
       </section>
     );
   }
 }
+
+export default App;
--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,5 +1,31 @@
 import css from './ContactsList.module.css';
+import cssButton from '../ContactForm/ContactForm.module.css';
 
-export default function ContactsList() {
-  return <></>;
+export default function ContactsList({ contacts, onDelete }) {
+  return (
+    <ul className={css.contactsListContainer}>
+      {contacts.length ? (
+        contacts.map(contact => {
+          return (
+            <li className={css.contactsItem} key={contact.id}>
+              <span className={css.text}>
+                {contact.name}: {contact.number}
+              </span>
+              <button
+                className={cssButton.deleteButton}
+                type="button"
+                // onClick={() => {
+                //   onDelete(contact.id);
+                // }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })
+      ) : (
+        <span className={css.text}>There is no contact in your phonebook</span>
+      )}
+    </ul>
+  );
 }
